0142025de8c78d085170cac14d966f38e307a31b
anflowersgo822294-hash <anflowersgo822294@gmail.com>
2025-11-27 00:50:52 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 # app.py
 import streamlit as st
-from modulos.ahorro import mostrar_ahorro  # Importamos la función mostrar_ahorro del módulo venta
 from modulos.login import login
-# Llamamos a la función mostrar_ahorro para mostrar el mensaje en la app
-mostrar_ahorro()
-login()
+from modulos.ahorro import mostrar_ahorro  # Importamos la función mostrar_ahorro del módulo ahorro
+
+# Primero ejecutamos el login
+if login():
+    # Si el login fue exitoso, mostramos el contenido de ahorro
+    mostrar_ahorro()
+else:
+    st.warning("Acceso denegado. Por favor, inicia sesión correctamente.")
